121c5d357ef035c81140cc21f9855ad340f3cd18
Justin Leblanc <justin.leblanc.pro@gmail.com>
2021-09-15 14:32:48 -0400

    Implémentation logique de partie gagnée et partie nulle
    .

--- app/src/main/java/dm/a21/tictactoetp1/JeuTTT.java
+++ app/src/main/java/dm/a21/tictactoetp1/JeuTTT.java
@@ -1,7 +1,12 @@
 package dm.a21.tictactoetp1;
 
+import android.widget.ImageView;
+
 public class JeuTTT extends ModeleJeu {
-    Joueur joueur;
+    private Joueur joueur;
+    private int iNbToursJoués = 0;
+    private boolean bPartieTerminée = false;
+
     public void changementTourDeJeu () {
         if(joueur == Joueur.O)
             joueur = Joueur.X;
@@ -13,5 +18,58 @@ public class JeuTTT extends ModeleJeu {
     }
     public void initialiserTour () {
         joueur = Joueur.X;
+    }
+    public void tourJoué () { iNbToursJoués++; }
+
+    public String Logique (char[] casesJeu) {
+        //X = 700068
+        //O = 700061
+        if(casesJeu[0] != '\u0000' && casesJeu[0] == casesJeu[1] && casesJeu[1] == casesJeu[2])
+        {
+            //Ligne Horizontale #1
+            return "Partie Gagnée par " + Character.toString(casesJeu[0]);
+        }
+        else if (casesJeu[3] != '\u0000' && casesJeu[3] == casesJeu[4] && casesJeu[4] == casesJeu[5])
+        {
+            //Ligne Horizontale #2
+            return "Partie Gagnée par " + Character.toString(casesJeu[3]);
+        }
+        else if (casesJeu[6] != '\u0000' && casesJeu[6] == casesJeu[7] && casesJeu[7] == casesJeu[8])
+        {
+            //Ligne Horizontale #3
+            return "Partie Gagnée par " + Character.toString(casesJeu[6]);
+        }
+        else if (casesJeu[0] != '\u0000' && casesJeu[0] == casesJeu[3] && casesJeu[3] == casesJeu[6])
+        {
+            //Ligne Verticale #1
+            return "Partie Gagnée par " + Character.toString(casesJeu[0]);
+        }
+        else if (casesJeu[1] != '\u0000' && casesJeu[1] == casesJeu[4] && casesJeu[4] == casesJeu[7])
+        {
+            //Ligne Verticale #2
+            return "Partie Gagnée par " + Character.toString(casesJeu[1]);
+        }
+        else if (casesJeu[2] != '\u0000' && casesJeu[2] == casesJeu[5] && casesJeu[5] == casesJeu[8])
+        {
+            //Ligne Verticale #3
+            return "Partie Gagnée par " + Character.toString(casesJeu[2]);
+        }
+        else if (casesJeu[0] != '\u0000' && casesJeu[0] == casesJeu[4] && casesJeu[4] == casesJeu[8])
+        {
+            //Diagonale #1
+            return "Partie Gagnée par " + Character.toString(casesJeu[0]);
+        }
+        else if (casesJeu[2] != '\u0000' && casesJeu[2] == casesJeu[4] && casesJeu[4] == casesJeu[6])
+        {
+            //Diagonale #2
+            return "Partie Gagnée par " + Character.toString(casesJeu[2]);
+        }
+        else
+        {
+            if(iNbToursJoués == 9)
+                return "Partie nulle!";
+            else
+                return "Rien";
+        }
     }
 }
--- app/src/main/java/dm/a21/tictactoetp1/MainActivity.java
+++ app/src/main/java/dm/a21/tictactoetp1/MainActivity.java
@@ -25,19 +25,17 @@ public class MainActivity extends AppCompatActivity implements InterfaceUtilisat
     ImageView imgCaseL3C2;
     ImageView imgCaseL3C3;
 
-    int[] cases = {R.id.ligne1colonne1, R.id.ligne1colonne2, R.id.ligne1colonne3,
-                   R.id.ligne2colonne1, R.id.ligne2colonne2, R.id.ligne2colonne3,
-                   R.id.ligne3colonne1, R.id.ligne3colonne2, R.id.ligne3colonne3};
+    //int[] cases = {R.id.ligne1colonne1, R.id.ligne1colonne2, R.id.ligne1colonne3,
+    //               R.id.ligne2colonne1, R.id.ligne2colonne2, R.id.ligne2colonne3,
+    //              R.id.ligne3colonne1, R.id.ligne3colonne2, R.id.ligne3colonne3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "Appel onCreate", Toast.LENGTH_SHORT).show();
-        Log.i("OnCreate", "OnCreateMainActivity");
 
         // Initialise le Joueur au tour de X
         jeu.initialiserTour();
-        Log.i("OnCreate", "initialiserTour");
 
         imgGrille = findViewById(R.id.grille);
         imgCaseL1C1 = findViewById(R.id.ligne1colonne1);
@@ -49,261 +47,230 @@ public class MainActivity extends AppCompatActivity implements InterfaceUtilisat
         imgCaseL3C1 = findViewById(R.id.ligne3colonne1);
         imgCaseL3C2 = findViewById(R.id.ligne3colonne2);
         imgCaseL3C3 = findViewById(R.id.ligne3colonne3);
-        Log.i("OnCreate", "imgCases");
 
-        ImageView[] cases = {imgCaseL1C1, imgCaseL1C2, imgCaseL1C3,
-                             imgCaseL2C1, imgCaseL2C2, imgCaseL2C3,
-                             imgCaseL3C1, imgCaseL3C2, imgCaseL3C3};
-        Log.i("OnCreate", "tableauImageView");
+        int iCaseL1C1 = 0;
+        int iCaseL1C2 = 1;
+        int iCaseL1C3 = 2;
+        int iCaseL2C1 = 3;
+        int iCaseL2C2 = 4;
+        int iCaseL2C3 = 5;
+        int iCaseL3C1 = 6;
+        int iCaseL3C2 = 7;
+        int iCaseL3C3 = 8;
+
+        ImageView[] casesImages = {imgCaseL1C1, imgCaseL1C2, imgCaseL1C3,
+                                   imgCaseL2C1, imgCaseL2C2, imgCaseL2C3,
+                                   imgCaseL3C1, imgCaseL3C2, imgCaseL3C3};
+
+        char cX = 'X';
+        char cO = 'O';
+        char[] casesJeu = new char[9];
 
         // Initialise toutes les cases du jeu à vide au lancement de l'application
-        for (int i = 0; i < cases.length; i++){
-            cases[i].setImageResource(R.drawable.vide);
+        for (int i = 0; i < casesImages.length; i++){
+            casesImages[i].setImageResource(R.drawable.vide);
         }
-        Log.i("OnCreate", "boucleFor");
-
-        //cases[3].
-        //Passer une position(case) et un jeton à jouer (X ou O)
-        //InterfaceUtilisateur.PlacerJetonDansCaseVide();
-
 
         imgCaseL1C1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"onClickListener", Toast.LENGTH_SHORT).show();
-                if (imgCaseL1C1.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL1C1.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
-                    Toast.makeText(getApplicationContext(),"if-Vide", Toast.LENGTH_SHORT).show();
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
-                        Toast.makeText(getApplicationContext(),"tourX", Toast.LENGTH_SHORT).show();
-                       imgCaseL1C1.setImageResource(R.drawable.x);
-                       jeu.changementTourDeJeu();
+                        imgCaseL1C1.setImageResource(R.drawable.x);
+                        casesJeu[iCaseL1C1] = cX;
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"tourO", Toast.LENGTH_SHORT).show();
                         imgCaseL1C1.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL1C1] = cO;
                     }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();
                 }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
         imgCaseL1C2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (imgCaseL1C2.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL1C2.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
                         imgCaseL1C2.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL1C2] = cX;
                     }
                     else
                     {
                         imgCaseL1C2.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL1C2] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
 
         imgCaseL1C3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (imgCaseL1C3.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL1C3.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
                         imgCaseL1C3.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL1C1] = cX;
                     }
                     else
                     {
                         imgCaseL1C3.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL1C3] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
         imgCaseL2C1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"onClickListener", Toast.LENGTH_SHORT).show();
-                if (imgCaseL2C1.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL2C1.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
-                    Toast.makeText(getApplicationContext(),"if-Vide", Toast.LENGTH_SHORT).show();
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
-                        Toast.makeText(getApplicationContext(),"tourX", Toast.LENGTH_SHORT).show();
                         imgCaseL2C1.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL2C1] = cX;
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"tourO", Toast.LENGTH_SHORT).show();
                         imgCaseL2C1.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL2C1] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
         imgCaseL2C2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"onClickListener", Toast.LENGTH_SHORT).show();
-                if (imgCaseL2C2.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL2C2.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
-                    Toast.makeText(getApplicationContext(),"if-Vide", Toast.LENGTH_SHORT).show();
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
-                        Toast.makeText(getApplicationContext(),"tourX", Toast.LENGTH_SHORT).show();
                         imgCaseL2C2.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL2C2] = cX;
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"tourO", Toast.LENGTH_SHORT).show();
                         imgCaseL2C2.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL2C2] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
         imgCaseL2C3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"onClickListener", Toast.LENGTH_SHORT).show();
-                if (imgCaseL2C3.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL2C3.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
-                    Toast.makeText(getApplicationContext(),"if-Vide", Toast.LENGTH_SHORT).show();
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
-                        Toast.makeText(getApplicationContext(),"tourX", Toast.LENGTH_SHORT).show();
                         imgCaseL2C3.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL2C3] = cX;
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"tourO", Toast.LENGTH_SHORT).show();
                         imgCaseL2C3.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL2C3] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
         imgCaseL3C1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"onClickListener", Toast.LENGTH_SHORT).show();
-                if (imgCaseL3C1.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL3C1.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
-                    Toast.makeText(getApplicationContext(),"if-Vide", Toast.LENGTH_SHORT).show();
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
-                        Toast.makeText(getApplicationContext(),"tourX", Toast.LENGTH_SHORT).show();
                         imgCaseL3C1.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL3C1] = cX;
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"tourO", Toast.LENGTH_SHORT).show();
                         imgCaseL3C1.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL3C1] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
         imgCaseL3C2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"onClickListener", Toast.LENGTH_SHORT).show();
-                if (imgCaseL3C2.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL3C2.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
-                    Toast.makeText(getApplicationContext(),"if-Vide", Toast.LENGTH_SHORT).show();
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
-                        Toast.makeText(getApplicationContext(),"tourX", Toast.LENGTH_SHORT).show();
                         imgCaseL3C2.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL3C2] = cX;
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"tourO", Toast.LENGTH_SHORT).show();
                         imgCaseL3C2.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL3C2] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
         imgCaseL3C3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"onClickListener", Toast.LENGTH_SHORT).show();
-                if (imgCaseL3C3.getDrawable().getConstantState() == MainActivity.this
-                        .getResources().getDrawable(R.drawable.vide).getConstantState())
+                if (imgCaseL3C3.getDrawable().getConstantState() == MainActivity.this.getResources().getDrawable(R.drawable.vide).getConstantState())
                 {
-                    Toast.makeText(getApplicationContext(),"if-Vide", Toast.LENGTH_SHORT).show();
                     if(jeu.tourDeJeuActuel() == ModeleJeu.Joueur.X)
                     {
-                        Toast.makeText(getApplicationContext(),"tourX", Toast.LENGTH_SHORT).show();
                         imgCaseL3C3.setImageResource(R.drawable.x);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL3C3] = cX;
                     }
                     else
                     {
-                        Toast.makeText(getApplicationContext(),"tourO", Toast.LENGTH_SHORT).show();
                         imgCaseL3C3.setImageResource(R.drawable.o);
-                        jeu.changementTourDeJeu();
+                        casesJeu[iCaseL3C3] = cO;
                     }
-                }
+                    jeu.changementTourDeJeu();
+                    jeu.tourJoué();
+                    Toast.makeText(getApplicationContext(),jeu.Logique(casesJeu),Toast.LENGTH_SHORT).show();                }
                 else
-                {
                     Toast.makeText(getApplicationContext(), R.string.caseRemplie, Toast.LENGTH_SHORT).show();
-                }
             }
         });
-
     }
     @Override
     protected void onStart()
@@ -341,8 +308,8 @@ public class MainActivity extends AppCompatActivity implements InterfaceUtilisat
     }
 
 
-    @Override
-    public void PlacerJetonDansCaseVide(String position, ImageView image, String joueur) {
-        
-    }
+    //@Override
+    //public void PlacerJetonDansCaseVide(String position, ImageView image, String joueur) {
+    //
+    //}
 }
